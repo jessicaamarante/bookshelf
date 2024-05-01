aa3a391c3a0635e6aecaa33d9b8c81d9b263760f
jessicaamarante <jessicaamarante1@gmail.com>
2024-05-01 19:50:11 -0300

    connection with API books

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,17 @@
+require 'faraday'
+
 class BooksController < ApplicationController
 
-def index
-end
+  def index
+    connection = Faraday.new(url: 'http://gutendex.com')
+    response = connection.get('/books/')
+    body_json = JSON.parse(response.body)
+    debugger
+
+    @books = body_json['results']
+
+    @books.each do |book|
+      title = book['title']
+    end
+  end
 end
