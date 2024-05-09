3b14eb11a16f304d892e1b1f0f4a3375e94dbea8
jessicaamarante <jessicaamarante1@gmail.com>
2024-05-09 20:36:45 -0300

    show only title and author

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,12 +6,17 @@ class BooksController < ApplicationController
     connection = Faraday.new(url: 'http://gutendex.com')
     response = connection.get('/books/')
     body_json = JSON.parse(response.body)
-    debugger
 
     @books = body_json['results']
-
+    @books_info = []
+    
     @books.each do |book|
-      title = book['title']
+      book_info = {
+        title: book['title'],
+        author: book['authors']
+      }
+      @books_info << book_info
+      debugger
     end
   end
 end
