b103571577a71c2cfc5b71b45c2e7ef95cd50b2c
jessicaamarante <jessicaamarante1@gmail.com>
2024-05-11 17:46:02 -0300

    Revert "show only title and author"
    This reverts commit 3b14eb11a16f304d892e1b1f0f4a3375e94dbea8.

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,17 +6,12 @@ class BooksController < ApplicationController
     connection = Faraday.new(url: 'http://gutendex.com')
     response = connection.get('/books/')
     body_json = JSON.parse(response.body)
+    debugger
 
     @books = body_json['results']
-    @books_info = []
-    
+
     @books.each do |book|
-      book_info = {
-        title: book['title'],
-        author: book['authors']
-      }
-      @books_info << book_info
-      debugger
+      title = book['title']
     end
   end
 end
