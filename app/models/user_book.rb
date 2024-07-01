66879f9cc7d1da233d9c1620b3331fdc834b7900
jessicaamarante <jessicaamarante1@gmail.com>
2024-06-30 21:51:41 -0300

    create method to get book_ids by user

--- app/models/user_book.rb
+++ app/models/user_book.rb
@@ -1,3 +1,17 @@
 class UserBook < ApplicationRecord
-    validates :user_id, :book_id, :status, presence: true
+  validates :user_id, :book_id, :status, presence: true
+  class << self
+    def get_books(current_user)
+      books = []
+      user = current_user
+
+      books_by_user = UserBook.where("user_id = ?", user)
+
+      books_by_user.each do |book|
+        books << book.book_id
+      end
+
+      books
+    end
+  end
 end
