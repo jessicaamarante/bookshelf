8ce3f8bb4dce5c3c3a5e2263f87142f821281158
jessicaamarante <jessicaamarante1@gmail.com>
2024-10-22 21:38:48 -0300

    add username for Users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_09_17_212201) do
+ActiveRecord::Schema[7.0].define(version: 2024_10_23_003716) do
   create_table "books", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -40,6 +40,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_09_17_212201) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
