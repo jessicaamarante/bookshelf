95f0585fe25265ddb603f6a9b763ca33e3001174
jessicaamarante <jessicaamarante1@gmail.com>
2024-10-26 07:26:53 -0300

    feat: add associaton for Profile and UserFriendship and create a follow method

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,17 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+has_one :profile
+
+has_many :friendships,
+          foreign_key: :user_id,
+          class_name: 'UserFriendship',
+          dependent: :destroy
+
+  def follow(friend)
+    unless self.friendships.exists?(friend_id: friend.id)
+      self.friendships.create(friend_id: friend.id)
+    end
+  end
 end
