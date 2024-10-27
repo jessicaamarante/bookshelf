16dc40a9aca06072558fd2c28974cca4e3e3e209
jessicaamarante <jessicaamarante1@gmail.com>
2024-10-27 18:33:09 -0300

    fix: change association name feat: add follow method to create a user_friendship relation when call follow method in profile

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,16 +4,17 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-has_one :profile
+  has_one :profile
 
-has_many :friendships,
-          foreign_key: :user_id,
-          class_name: 'UserFriendship',
-          dependent: :destroy
+  has_many :user_friendships, foreign_key: :user_id, class_name: 'UserFriendship', dependent: :destroy
+
+  # defines a many-to-many relationship between users through the user_friendships join table, allowing you to access a user's friends directly.
+  # if i don't have this line, i can't access friends directly from a user, i have to query the user_friendships table to get the friends.
+  has_many :friends, through: :user_friendships, source: :friend
 
   def follow(friend)
-    unless self.friendships.exists?(friend_id: friend.id)
-      self.friendships.create(friend_id: friend.id)
+    unless self.user_friendships.exists?(friend_id: friend.id)
+      self.user_friendships.create(friend_id: friend.id)
     end
   end
 end
--- app/models/user_friendship.rb
+++ app/models/user_friendship.rb
@@ -1,4 +1,5 @@
 class UserFriendship < ApplicationRecord
+  # the UserFriendship model belongs to a user and a friend (which is also a User).
   belongs_to :user
   belongs_to :friend, class_name: 'User', foreign_key: :friend_id
 end
