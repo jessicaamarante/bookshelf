7263625e8a60c8fd8821cb036420bfa0b1d01b35
jessicaamarante <jessicaamarante1@gmail.com>
2024-11-06 20:14:13 -0300

    feat: add condition to show button unfollow if user already following that friend

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -5,8 +5,10 @@ class ProfileController < ApplicationController
   def index
     if params[:email].present?
       @friends = User.where(email: params[:email])
+      @following = @friends.select { |friend| current_user.user_friendships.exists?(friend_id: friend.id) }
     else
       @friends = []
+      @following = []
     end
   end
 
