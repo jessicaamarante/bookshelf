b3ded4d43ca26c54bd9066300677343e7abf503b
jessicaamarante <jessicaamarante1@gmail.com>
2024-11-27 20:53:00 -0300

    add validates to user friendship

--- app/models/user_friendship.rb
+++ app/models/user_friendship.rb
@@ -2,4 +2,6 @@ class UserFriendship < ApplicationRecord
   # the UserFriendship model belongs to a user and a friend (which is also a User).
   belongs_to :user
   belongs_to :friend, class_name: 'User', foreign_key: :friend_id
+
+  validates_presence_of :friend_id, :user_id
 end
